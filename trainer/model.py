09f4bd71335fe69f2e99be0487b48e8c6873917c
Frank Kloster <frank.kloster@gmail.com>
2019-11-10 20:20:21 -0800

    Moved some functionality to model.py

---
+++ trainer/model.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import time
+import os
+import copy
+
+def build_model(device='cpu'):
+    model_ft = models.resnet18(pretrained=True)
+
+    # Attach a a final layer.
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, 2)
+
+    model_ft = model_ft.to(device)
+
+    return model_ft
--- trainer/task.py
+++ trainer/task.py
@@ -9,6 +9,8 @@ import time
 import os
 import copy
 
+from trainer.model import build_model
+
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
     since = time.time()
 
@@ -105,13 +107,7 @@ if __name__ == '__main__':
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    model_ft = models.resnet18(pretrained=True)
-    num_ftrs = model_ft.fc.in_features
-    # Here the size of each output sample is set to 2.
-    # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
-    model_ft.fc = nn.Linear(num_ftrs, 2)
-
-    model_ft = model_ft.to(device)
+    model_ft = build_model(device=device)
 
     criterion = nn.CrossEntropyLoss()
 
